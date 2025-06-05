4d394ee2fd9080f22b92520f23430fa0cd01c0ef
Daniel Dinev <azsumbg@gmail.com>
2025-06-05 21:25:47 +0300

    Add project files.

---
+++ SeaDll/SeaDll.cpp
@@ -0,0 +1,600 @@
+#include "pch.h"
+#include "SeaDll.h"
+
+// RANDIt ***********************
+
+dll::RANDIt::RANDIt()
+{
+	std::random_device rd{};
+	std::seed_seq sq{ rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd() };
+
+	twister = new std::mt19937(sq);
+}
+dll::RANDIt::~RANDIt()
+{
+	if (twister)delete twister;
+}
+
+int dll::RANDIt::operator()(int min, int max)
+{
+	std::uniform_int_distribution distrib(min, max);
+
+	return distrib(*twister);
+}
+
+/////////////////////////////////
+
+// PPROTON ****************************************
+
+dll::PROTON::PROTON(float _sx, float _sy, float _width, float _height)
+{
+	start.x = _sx;
+	start.y = _sy;
+
+	width = _width;
+	height = _height;
+
+	end.x = start.x + width;
+	end.y = start.y + height;
+
+	x_radius = width / 2;
+	y_radius = height / 2;
+
+	center.x = start.x + x_radius;
+	center.y = start.y + y_radius;
+}
+dll::PROTON::PROTON(FPOINT _start_pos, float _width, float _height)
+{
+	start = _start_pos;
+	
+	width = _width;
+	height = _height;
+
+	end.x = start.x + width;
+	end.y = start.y + height;
+
+	x_radius = width / 2;
+	y_radius = height / 2;
+
+	center.x = start.x + x_radius;
+	center.y = start.y + y_radius;
+}
+
+float dll::PROTON::GetWidth()const
+{
+	return width;
+}
+float dll::PROTON::GetHeight()const
+{
+	return height;
+}
+float dll::PROTON::XRadius()const
+{
+	return x_radius;
+}
+float dll::PROTON::YRadius()const
+{
+	return y_radius;
+}
+
+void dll::PROTON::SetEdges()
+{
+	end.x = start.x + width;
+	end.y = start.y + height;
+
+	center.x = start.x + x_radius;
+	center.y = start.y + y_radius;
+}
+void dll::PROTON::NewDims(float _new_width, float _new_height)
+{
+	width = _new_width;
+	height = _new_height;
+
+	end.x = start.x + width;
+	end.y = start.y + height;
+
+	x_radius = width / 2;
+	y_radius = height / 2;
+
+	center.x = start.x + x_radius;
+	center.y = start.y + y_radius;
+}
+
+void dll::PROTON::NewWidth(float _new_width)
+{
+	width = _new_width;
+	
+	end.x = start.x + width;
+	
+	x_radius = width / 2;
+	
+	center.x = start.x + x_radius;
+}
+void dll::PROTON::NewHeight(float _new_height)
+{
+	height = _new_height;
+
+	end.y = start.y + height;
+
+	y_radius = height / 2;
+
+	center.y = start.y + y_radius;
+}
+
+///////////////////////////////////////////////////
+
+// OBJECT CLASS ***********************************
+
+dll::OBJECT::OBJECT(types _what, float _to_x, float _to_y) :PROTON(_to_x, _to_y, 1.0f, 1.0f)
+{
+	type = _what;
+
+	switch (type)
+	{
+	case types::field:
+		NewDims(1000.0f, 750.0f);
+		max_frames = 75;
+		frame_delay = 0;
+		break;
+
+	case types::evil1:
+		NewDims(120.0f, 69.0f);
+		max_frames = 49;
+		frame_delay = 1;
+		break;
+
+	case types::evil2:
+		NewDims(130.0f, 85.0f);
+		max_frames = 12;
+		frame_delay = 6;
+		break;
+
+	case types::evil3:
+		NewDims(174.0f, 100.0f);
+		max_frames = 6;
+		frame_delay = 10;
+		break;
+
+	case types::evil4:
+		NewDims(80.0f, 60.0f);
+		max_frames = 20;
+		frame_delay = 3;
+		break;
+
+	case types::evil5:
+		NewDims(65.0f, 70.0f);
+		max_frames = 8;
+		frame_delay = 9;
+		break;
+
+	case types::evil6:
+		NewDims(59.0f, 50.0f);
+		max_frames = 40;
+		frame_delay = 1;
+		break;
+
+	case types::evil7:
+		NewDims(68.0f, 40.0f);
+		max_frames = 12;
+		frame_delay = 6;
+		break;
+
+	case types::evil8:
+		NewDims(75.0f, 76.0f);
+		max_frames = 16;
+		frame_delay = 6;
+		break;
+
+	case types::jelly:
+		NewDims(66.0f, 80.0f);
+		max_frames = 4;
+		frame_delay = 16;
+		break;
+
+	case types::hero:
+		NewDims(120.0f, 100.0f);
+		max_frames = 2;
+		frame_delay = 35;
+		break;
+
+
+	}
+}
+
+types dll::OBJECT::GetType() const
+{
+	return type;
+}
+int dll::OBJECT::GetFrame()
+{
+	frame_delay--;
+	if (frame_delay <= 0)
+	{
+		switch (type)
+		{
+		case types::field:
+			frame_delay = 0;
+			break;
+
+		case types::evil1:
+			frame_delay = 1;
+			break;
+
+		case types::evil2:
+			frame_delay = 6;
+			break;
+
+		case types::evil3:
+			frame_delay = 10;
+			break;
+
+		case types::evil4:
+			frame_delay = 3;
+			break;
+
+		case types::evil5:
+			frame_delay = 9;
+			break;
+
+		case types::evil6:
+			frame_delay = 1;
+			break;
+
+		case types::evil7:
+			frame_delay = 6;
+			break;
+
+		case types::evil8:
+			frame_delay = 6;
+			break;
+
+		case types::jelly:
+			frame_delay = 16;
+			break;
+
+		case types::hero:
+			frame_delay = 35;
+			break;
+		}
+
+		++current_frame;
+		if (current_frame >= max_frames)current_frame = 0;
+	}
+
+	return current_frame;
+}
+void dll::OBJECT::SetType(types what)
+{
+	type = what;
+	current_frame = 0;
+
+	switch (type)
+	{
+	case types::field:
+		NewDims(1000.0f, 750.0f);
+		max_frames = 75;
+		frame_delay = 0;
+		break;
+
+	case types::evil1:
+		NewDims(120.0f, 69.0f);
+		max_frames = 49;
+		frame_delay = 1;
+		break;
+
+	case types::evil2:
+		NewDims(130.0f, 85.0f);
+		max_frames = 12;
+		frame_delay = 6;
+		break;
+
+	case types::evil3:
+		NewDims(174.0f, 100.0f);
+		max_frames = 6;
+		frame_delay = 10;
+		break;
+
+	case types::evil4:
+		NewDims(80.0f, 60.0f);
+		max_frames = 20;
+		frame_delay = 3;
+		break;
+
+	case types::evil5:
+		NewDims(65.0f, 70.0f);
+		max_frames = 8;
+		frame_delay = 9;
+		break;
+
+	case types::evil6:
+		NewDims(59.0f, 50.0f);
+		max_frames = 40;
+		frame_delay = 1;
+		break;
+
+	case types::evil7:
+		NewDims(68.0f, 40.0f);
+		max_frames = 12;
+		frame_delay = 6;
+		break;
+
+	case types::evil8:
+		NewDims(75.0f, 76.0f);
+		max_frames = 16;
+		frame_delay = 6;
+		break;
+
+	case types::jelly:
+		NewDims(66.0f, 80.0f);
+		max_frames = 4;
+		frame_delay = 16;
+		break;
+
+	case types::hero:
+		NewDims(120.0f, 100.0f);
+		max_frames = 2;
+		frame_delay = 35;
+		break;
+	}
+}
+void dll::OBJECT::ObjRelease()
+{
+	delete this;
+}
+
+///////////////////////////////////////////////////
+
+// CREATURES **************************************
+
+dll::CREATURES::CREATURES(types __what, float __start_x, float __start_y, float __to_x, float __to_y)
+	:OBJECT(__what, __start_x, __start_y)
+{
+	if (type != types::hero)SetPathInfo(__to_x, __to_y);
+
+	switch (type)
+	{
+	case types::evil1:
+		speed = 0.7f;
+		sight_scope = 100.0f;
+		break;
+
+	case types::evil2:
+		speed = 0.6f;
+		sight_scope = 110.0f;
+		break;
+
+	case types::evil3:
+		speed = 0.5f;
+		sight_scope = 130.0f;
+		break;
+
+	case types::evil4:
+		speed = 0.8f;
+		sight_scope = 90.0f;
+		break;
+
+	case types::evil5:
+		speed = 0.9f;
+		sight_scope = 90.0f;
+		break;
+
+	case types::evil6:
+		speed = 1.0f;
+		sight_scope = 70.0f;
+		break;
+
+	case types::evil7:
+		speed = 1.2f;
+		sight_scope = 50.0f;
+		break;
+
+	case types::evil8:
+		speed = 0.9f;
+		sight_scope = 80.0f;
+		break;
+
+	case types::jelly:
+		speed = 0.6f;
+		break;
+
+	case types::hero:
+		speed = 5.0f;
+		break;
+	}
+}
+void dll::CREATURES::SetPathInfo(float _to_where_x, float _to_where_y)
+{
+	move_sx = start.x;
+	move_sy = start.y;
+	move_ex = _to_where_x;
+	move_ey = _to_where_y;
+
+	hor_dir = false;
+	ver_dir = false;
+
+	if (move_sx == move_ex || (move_ex > start.x && move_ex <= end.x))
+	{
+		ver_dir = true;
+		return;
+	}
+	if (move_sy == move_ey || (move_ey > start.y && move_ey <= end.y))
+	{
+		hor_dir = true;
+		return;
+	}
+
+	slope = (move_ey - move_sy) / (move_ex - move_sx);
+	intercept = move_sy - move_sx * slope;
+}
+bool dll::CREATURES::Move(float gear)
+{
+	float my_speed = speed + gear / 10.0f;
+
+	if (type != types::hero)return false;
+
+	switch (dir)
+	{
+	case dirs::right: 
+		if (end.x + my_speed <= scr_width)
+		{
+			start.x += my_speed;
+			SetEdges();
+			return true;
+		}
+		break;
+
+	case dirs::left:
+		if (start.x - my_speed >= 0)
+		{
+			start.x -= my_speed;
+			SetEdges();
+			return true;
+		}
+		break;
+
+	case dirs::down:
+		if (end.y + my_speed <= scr_height)
+		{
+			start.y += my_speed;
+			SetEdges();
+			return true;
+		}
+		break;
+
+	case dirs::up:
+		if (start.y - my_speed >= 50.0f)
+		{
+			start.y -= my_speed;
+			SetEdges();
+			return true;
+		}
+		break;
+	}
+
+	return false;
+}
+bool dll::CREATURES::Move(float gear, float dest_x, float dest_y)
+{
+	float my_speed = speed + gear / 10.0f;
+
+	SetPathInfo(dest_x, dest_y);
+
+	if (ver_dir)
+	{
+		if (end.y + my_speed <= scr_height)
+		{
+			start.y += my_speed;
+			SetEdges();
+			if (move_ey <= center.y)return false;
+			return true;
+		}
+		if (start.y - my_speed >= 50.0f)
+		{
+			start.y -= my_speed;
+			SetEdges();
+			if (move_ey >= center.y)return false;
+			return true;
+		}
+	}
+	else if (hor_dir)
+	{
+		if (end.x + my_speed <= scr_width)
+		{
+			start.x= my_speed;
+			SetEdges();
+			if (move_ex <= center.x)return false;
+			return true;
+		}
+		if (start.x - my_speed >= 0)
+		{
+			start.x -= my_speed;
+			SetEdges();
+			if (move_ex >= center.x)return false;
+			return true;
+		}
+	}
+	else
+	{
+		if (move_ex > move_sx)
+		{
+			if (end.x + my_speed <= scr_width)
+			{
+				start.x = my_speed;
+				start.y = start.x * slope + intercept;
+				SetEdges();
+				if (move_ex <= center.x)return false;
+				return true;
+			}
+		}
+		else if (move_ex < move_sx)
+		{
+			if (start.x - my_speed >= 0)
+			{
+				start.x -= my_speed;
+				start.y = start.x * slope + intercept;
+				SetEdges();
+				if (move_ex >= center.x)return false;
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+void dll::CREATURES::EvilAI(PACK<FPOINT>& pack, float gear)
+{
+	state = states::stop;
+	Sort(pack, center);
+
+	FPOINT target = pack.front();
+
+	if (Distance(center, target) <= sight_scope)
+	{
+		state = states::move;
+		if (center.x < target.x)dir = dirs::right;
+		else dir = dirs::left;
+
+		if (!Move(gear, target.x, target.y))state = states::patrol;
+	}
+	else
+	{
+		if (center.x <= scr_width / 2)dir = dirs::right;
+		else dir = dirs::left;
+		--patrol_move_points;
+		if (dir == dirs::right)
+		{
+			if (patrol_move_points <= 0)
+			{
+				patrol_move_points = 200;
+				dir = dirs::left;
+				return;
+			}
+			if (!Move(gear))
+			{
+				dir = dirs::left;
+				return;
+			}
+		}
+	}
+}
+void dll::CREATURES::Release()
+{
+	delete this;
+}
+
+///////////////////////////////////////////////////
+
+// FUNCTIONS ************************************
+
+float dll::Distance(FPOINT my_point, FPOINT target_point)
+{
+	float a = (float)(pow(abs(target_point.x - my_point.x), 2));
+	float b = (float)(pow(abs(target_point.y - my_point.y), 2));
+
+	return (float)(sqrt(a + b));
+}
+
+
+
+
+////////////////////////////////////////////////
---
+++ SeaDll/SeaDll.h
@@ -0,0 +1,290 @@
+#pragma once
+
+#ifdef SEADLL_EXPORTS
+#define SEADLL_API __declspec(dllexport)
+#else
+#define SEADLL_API __declspec(dllimport)
+#endif
+
+#include <random>
+
+constexpr float scr_width{ 1000.0f };
+constexpr float scr_height{ 800.0f };
+
+constexpr float sky{ 50.0f };
+constexpr float ground{ 750.0f };
+
+enum class types {
+	evil1 = 0, evil2 = 1, evil3 = 2, evil4 = 3, evil5 = 4,
+	evil6 = 5, evil7 = 6, evil8 = 7, hero = 8, jelly = 9, field = 10, no_type = 11
+};
+
+enum class dirs { stop = 0, left = 1, right = 2, up = 3, down = 4 };
+
+enum class states { move = 0, patrol = 1, stop = 2 };
+
+struct FPOINT
+{
+	float x{ 0 };
+	float y{ 0 };
+};
+
+namespace dll
+{
+	class SEADLL_API RANDIt
+	{
+	private:
+		std::mt19937* twister{ nullptr };
+
+	public:
+		RANDIt();
+		~RANDIt();
+
+		int operator()(int min, int max);
+	};
+
+	template<typename T> class SEADLL_API PACK
+	{
+	private:
+		T* m_ptr{ nullptr };
+		size_t max_size{ 0 };
+		size_t next_pos{ 0 };
+
+		bool is_valid{ false };
+
+	public:
+		PACK() :max_size{ 1 }, m_ptr{ reinterpret_cast<T*>(calloc(1,sizeof(T)) }
+		{
+			if (m_ptr)is_valid = true;
+		}
+		PACK(size_t lenght) :max_size{ lenght }, m_ptr{ reinterpret_cast<T*>(calloc(max_size,sizeof(T))) }
+		{
+			if (m_ptr)is_valid = true;
+		}
+		~PACK()
+		{
+			if (m_ptr)free(m_ptr);
+		}
+
+		bool valid() const
+		{
+			return is valid;
+		}
+		size_t capacity() const
+		{
+			return max_size;
+		}
+		size_t size() const
+		{
+			return next_pos;
+		}
+
+		void push_back(T element)
+		{
+			if (!is_valid)return;
+
+			if (next_pos < max_size)
+			{
+				m_ptr[next_pos] = element;
+				++next_pos;
+				return;
+			}
+			else
+			{
+				m_ptr = reinterpret_cast<T*>(realloc(m_ptr, (max_size + 1) * sizeof(T)));
+				m_ptr[next_pos] = element;
+
+				++max_size;
+				++next_pos;
+				return;
+			}
+		}
+		void push_front(T element)
+		{
+			if (!is_valid)return;
+			*m_ptr = element;
+		}
+		void insert(size_t index, T element)
+		{
+			if (!is_valid || index >= max_size)return;
+
+			if (next_pos < max_size)
+			{
+				T* temp_ptr = reinterpret_cast<T*>(calloc(max_size), sizeof(T));
+
+				for (size_t i = 0; i < max_size; ++i)
+				{
+					if (i < index)temp_ptr[i] = m_ptr[i];
+					else if (i == index)temp_ptr[i] = element;
+					else temp_ptr[i + 1] = m_ptr[i];
+				}
+
+				free(m_ptr);
+				m_ptr = temp_ptr;
+				++next_pos;
+				return;
+			}
+			else
+			{
+				T* temp_ptr = reinterpret_cast<T*>(calloc(max_size + 1), sizeof(T));
+
+				for (size_t i = 0; i < max_size; ++i)
+				{
+					if (i < index)temp_ptr[i] = m_ptr[i];
+					else if (i == index)temp_ptr[i] = element;
+					else temp_ptr[i + 1] = m_ptr[i];
+				}
+
+				free(m_ptr);
+				m_ptr = temp_ptr;
+				++next_pos;
+				return;
+			}
+
+		}
+
+		T& front()
+		{
+			return m_ptr[0];
+		}
+		T& back()
+		{
+			return m_ptr[next_pos - 1];
+		}
+
+		T& operator[](size_t index)
+		{
+			T dummy{};
+
+			if (!is_valid || index >= next_pos)return dummy;
+
+			return m_ptr[index];
+		}
+	};
+
+	class SEADLL_API PROTON
+	{
+	protected:
+		float width{ 1.0f };
+		float height{ 1.0f };
+
+		float x_radius{ 0 };
+		float y_radius{ 0 };
+
+	public:
+		FPOINT start{ 0,0 };
+		FPOINT end{ 0,0 };
+		FPOINT center{ 0,0 };
+
+		PROTON() {};
+		PROTON(float _sx, float _sy, float _width, float _height);
+		PROTON(FPOINT _start_pos, float _width, float _height);
+
+		virtual ~PROTON() {};
+
+		float GetWidth()const;
+		float GetHeight()const;
+		float XRadius()const;
+		float YRadius()const;
+
+		void SetEdges();
+		void NewDims(float _new_width, float _new_height);
+
+		void NewWidth(float _new_width);
+		void NewHeight(float _new_height);
+	};
+
+	class SEADLL_API OBJECT :public PROTON
+	{
+	protected:
+		types type = types::no_type;
+
+		int current_frame{ 0 };
+		int max_frames{ 0 };
+		int frame_delay{ 0 };
+
+		OBJECT(types _what, float _to_x, float _to_y);
+
+	public:
+		dirs dir = dirs::stop;
+		
+		virtual ~OBJECT() {};
+
+		types GetType() const;
+		int GetFrame();
+
+		void SetType(types what);
+		void ObjRelease();
+
+		friend SEADLL_API OBJECT* CreateObject(types what, float to_x, float to_y);
+	};
+
+	class SEADLL_API CREATURES :public OBJECT
+	{
+	private:
+
+		float move_sx{ 0 };
+		float move_sy{ 0 };
+		float move_ex{ 0 };
+		float move_ey{ 0 };
+
+		float slope{ 0 };
+		float intercept{ 0 };
+
+		float speed{ 0 };
+		
+		bool hor_dir = false;
+		bool ver_dir = false;
+
+		float sight_scope{ 0 };
+
+		int patrol_move_points = 200;
+
+		CREATURES(types __what, float __start_x, float __start_y, float __to_x, float __to_y);
+
+		void SetPathInfo(float _to_where_x, float _to_where_y);
+
+	public:
+		states state = states::stop;
+
+		friend SEADLL_API CREATURES* CreateCreature(types what, float start_x, float start_y, float to_x, float to_y);
+
+		bool Move(float gear);
+		bool Move(float gear, float dest_x, float dest_y);
+
+		void EvilAI(PACK<FPOINT>& pack, float gear);
+
+		void Release();
+	};
+
+
+	// FUNCTIONS ************************************
+
+	float SEADLL_API Distance(FPOINT my_point, FPOINT target_point);
+	
+	void Sort(PACK<FPOINT>& pack, FPOINT target)
+	{
+		bool sorted = false;
+
+		while (!sorted)
+		{
+			sorted = true;
+
+			for (size_t i = 0; i < pack.size() - 1; ++i)
+			{
+				if (Distance(pack[i], target) > Distance(pack[i + 1], target))
+				{
+					FPOINT temp = pack[i];
+					pack[i] = pack[i + 1];
+					pack[i + 1] = temp;
+					sorted = false;
+					break;
+				}
+			}
+		}
+	}
+
+	SEADLL_API OBJECT* CreateObject(types what, float to_x, float to_y);
+
+	SEADLL_API CREATURES* CreateCreature(types what, float start_x, float start_y, float to_x, float to_y);
+}
